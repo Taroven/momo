f0da3e24bdeb3392b5bb3bfbeb0fe7ca8c87c199
Ezra Sims <ezra.sims@gmail.com>
2014-09-06 13:04:12 -0400

    log: major unfuckery

--- log.lua
+++ log.lua
@@ -14,17 +14,17 @@ local logstack = {}
 local pop = function ()
 	if not errorlevel then return end
 	while stack[1] do
-		local entry = table.remove(log,1)
-		if entry.ref and (entry.level >= errorlevel) then
+		local entry = table.remove(stack,1)
+		if entry.obj and (entry.level >= errorlevel) then
 			if type(entry.msg) ~= "table" then entry.msg = {entry.msg} end
-			local r = entry.ref
-			local msg = errorlevels[entry.level] .. ": " .. r.caller .. (entry.method and ("." .. entry.method) or "") .. ": " .. table.concat(entry.msg, r.sep or " | ")
+			local r = entry.obj
+			local msg = errorlevels[entry.level] .. ": " .. r.__name .. (entry.method and ("." .. entry.method) or "") .. ": " .. table.concat(entry.msg, r.__sep or " | ")
 			MOAILogMgr.log(msg)
 		end
 	end
 end
 
-local push = function (self, method, level, ...)
+local push = function (obj, method, level, ...)
 	util.argcheck(self,1,"table")
 	local t = {...}
 	local entry = {}
@@ -34,36 +34,18 @@ local push = function (self, method, level, ...)
 	entry.msg = t
 	entry.level = type(level) == "number" and level or 2
 	entry.method = method
-	entry.ref = self
+	entry.obj = obj
 	
 	stack[#stack + 1] = entry
 	return pop()
 end
 
-local log = class("log")
-log.init = function (self,caller)
-	util.argcheck(caller,2,"string")
-	self.caller = caller
-	self.sep = " | "
-	local mt = getmetatable(self) or {}
-	mt.__call = function (self, method, level, ...) return self:log(method, level, ...) end
-	setmetatable(self,mt)
-	return self
-end
-
-log.log = function (self, method, level, ...)
-	return push(self, method, level, ...)
-end
-
-log.method = function (self,method)
-	local m = method
-	self._methods[m] = self._methods[m] or function (level, ...) return self:log(m, level, ...) end
-	return self._methods[m]
-end
+local log = {}
 
 log.SetLevel = function (level)
 	util.argcheck(level,1,"number","nil")
 	errorlevel = level or lastlevel
+	pop()
 end
 
 log.Disable = function ()
@@ -78,6 +60,11 @@ end
 log.File = function (path)
 	return MOAILogMgr.openFile(path)
 end
+
+local mt = {
+	__call = function (_, obj, method, level, ...) return push(obj, method, level, ...) end
+}
+setmetatable(log, mt)
 
 _LOG = log
 return log
