47c12ed3f0cc0e93d5fe0bc525e50e8a7d77869e
Ezra Sims <ezra.sims@gmail.com>
2014-10-05 00:23:24 +0000

    classmanager: take advantage of object properties for config needs instead of injecting rewritten methods. Now using lash.config instead of _G.configuration. viewport: refactor for new config scheme

--- classes/viewport.lua
+++ classes/viewport.lua
@@ -5,22 +5,22 @@
 --]]
 
 local c = class("viewport")
-local cfg = config.Get
+local cfg = class.config
 
 c.InitViewport = function (self)
 	self:Log(1,'InitViewport')
 	-- Use the config system to enable a singleton viewport
-	local viewport = cfg('viewport','viewport',MOAIViewport.new)
+local viewport = cfg:Get('viewport',MOAIViewport.new)
 	self:SafeSet("viewport",viewport)
 
-	local initialized = cfg('viewport','initialized')
+	local initialized = cfg:Get('viewport.initialized')
 	if not initialized then
 		self:Log(2,'InitViewport','First time viewport initialization in progress.')
-		viewport:setSize(cfg('viewport','sizeX',MOAIEnvironment.horizontalResolution),
-		                 cfg('viewport','sizeY',MOAIEnvironment.verticalResolution))
-		viewport:setScale(cfg('viewport','scaleX',MOAIEnvironment.horizontalResolution),
-		                  cfg('viewport','scaleY',MOAIEnvironment.verticalResolution)) 
-		config.Set('viewport','initialized',true)
+		viewport:setSize(cfg:Get('viewport.dimensions.x',MOAIEnvironment.horizontalResolution),
+		                 cfg:Get('viewport.dimensions.y',MOAIEnvironment.verticalResolution))
+		viewport:setScale(cfg:Get('viewport.scale.x',MOAIEnvironment.horizontalResolution),
+		                  cfg:Get('viewport.scale.y',MOAIEnvironment.verticalResolution)) 
+		cfg:Set('viewport.initialized',true)
 	end
 
 	return viewport
