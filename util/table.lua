7bddc2d402c0b76128e427190cf8dde3661bb1a4
Ezra Sims <ezra.sims@gmail.com>
2015-01-01 01:58:18 -0500

    Remove pointless util.table funcs

--- util/table.lua
+++ util/table.lua
@@ -10,45 +10,6 @@ function t.copy (src, dest, timid)
    return dest
 end
 
--- Deep copy. Functions and userdata are kept as pointers, but that's about it. Eats memory if abused.
--- Infinite recursion is avoided by an encounter table - if recursion is detected it just adds a reference.
--- Note: Shallow implies that tables are skipped completely (mostly for the sake of metatable sanity)
-function t.clone (src, dest, timid, shallow, enc)
-	enc = enc or {[tostring(_ENV or _G)] = (_ENV or _G)} -- recurse check
-	dest = type(dest) == "table" and dest or {}
-	for k,v in pairs(src) do
-		if type(v) == "table" and not shallow then
-			local e = tostring(v)
-			enc[e] = enc[e] or t.clone(v, dest[k], timid, nil, enc)
-			dest[k] = (timid and type(dest[k]) ~= "nil" and dest[k]) or enc[e]
-		else
-			dest[k] = (timid and type(dest[k]) ~= "nil" and dest[k]) or v
-		end
-	end
-	return dest
-end
-
--- Combine multiple tables into a single metaindex. Returns a setmetatable()-ready table with index.
--- Usage: setmetatable(target,t.shadow(t1,t2,...)) == setmetatable(target,{__index = {t1 .. t2 .. ...}})
--- Precedence is by arg order, so place your preferred values earlier in the arg stack.
-function t.shadow (...)
-	local src = {...}
-	local dmt = {}
-	local dest = {}
-	local enc = {[tostring(_ENV or _G)] = (_ENV or _G)} -- keep memory usage down a bit by preserving the encounter table for clone()
-
-	while src[1] do
-		local t = table.remove(src)
-		local mt = getmetatable(t)
-		if mt and mt.__index then
-			table.clone(mt.__index,dmt)
-		end
-		table.clone(t,dest,nil,nil,enc)
-	end
-	local final = table.clone(dest,dmt)
-	return {__index = final}
-end
-
 -- Sorted pairs - less efficient than pairs, but consistent results. Usage is identical to pairs (though iteration is numeric)
 -- Allows passing a custom sorting function (arg 2) for use as a priority iterator, reverse alpha, etc.
 function t.spairs (t,f)
