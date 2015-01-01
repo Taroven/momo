ddad78cc6c6cf10bb30c156c02ec3f78c289d368
Ezra Sims <ezra.sims@gmail.com>
2015-01-01 02:02:08 -0500

    More util trimming.

--- util/util.lua
+++ util/util.lua
@@ -2,28 +2,6 @@ local util = {}
 
 local type,assert,error,select = type,assert,error,select
 
--- Enhanced type example: type({},"string","table") == "table", 2
--- Useful for switch statements or basic arg checking (use util.argcheck for a strict approach)
-local otype = type
-util.type = function (value, ...)
-	local n = select("#", ...)
-	if n == 0 then return otype(value) end
-	for i=1,n do
-		if otype(value) == select(i, ...) then
-			return otype(value), i
-		end
-	end
-end
-
--- Macro for quick MD5 hashing.
-util.gethash = function (data)
-   local writer = MOAIHashWriter.new()
-   writer:openMD5()
-   writer:write(data)
-   writer:close()
-   return writer:getHashHex()
-end
-
 -- Throw an error if an arg's type doesn't match an intended type.
 -- Example: function foo (a, b, c) util.argcheck(a, 1, 'string'); DoStuff(a) end
 util.argcheck = function (value, num, ...)
